a8e780f5ce47cfc51c5889a01ef5844102333c04
Kweayon  Clark <Kweayon@gmail.com>
2022-07-12 12:09:52 -0500

    Update
    Updated comments and added stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -131,5 +131,35 @@ class _NinjaCardState extends State<NinjaCard> {
   }
 }
 
-
 // Creating a stateful widget from scratch
+// if you are in visual studio code you can use the snipit stful and press tab to create a stateful widget
+// you then need to rename your widget
+
+// Creates two classes for us
+
+// First it creates a class that extends/inherits from stateful widget
+
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  // override the createState function
+  // CreateState is instantiating the next class we build our state class object
+  // Builds a state object for the stateful widget
+
+  @override
+  State<Test> createState() => _TestState();
+}
+// The state object
+// Which is just another class
+// Can define data and change the state of the class or state object values over time
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    int counter = 1;
+    // Return widget tree
+    return Container(
+      child: Text('Counter: $counter'),
+    );
+  }
+}
